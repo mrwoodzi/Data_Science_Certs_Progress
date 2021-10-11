06ab3343b3d2582358e152899690e7ae0e16cb03
mrwoodzi <mrwoodzi@protonmail.com>
2021-10-11 13:14:09 -0500

    free_code_camp

--- strings.py
+++ strings.py
@@ -1,4 +1,27 @@
 for n in "banana":
     print(n)
 
+
+# using in as a logical operator
+
+fruit = 'banana'
+print(n in fruit)
+print('m' in fruit)
+print('nan' in fruit)
+if 'a' in fruit:
+    print('Found It!')
+
+# STRING COMPARISON
+if work == 'banana':
+    print('Alright, bananas')
+
+if word < 'banana':
+    print('Your word', + word + ', comes before banana.')
+elif word > 'banana':
+    print('Your word,' + word + ', comes after banan.')
+else:
+    print('All right, bananas.')
+
+
+
     
