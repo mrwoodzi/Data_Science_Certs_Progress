79d991d7a5f73193b39387c6a2a246462c2830bc
mrwoodzi <mrwoodzi@protonmail.com>
2021-10-12 13:24:01 -0500

    free_code_camp

---
+++ python_for_everybody/dictionaries.py
@@ -0,0 +1,17 @@
+# Dictionaries - A 'bag' of values, each with its own label
+#Dictionaries are pythons most power data collection tool
+#Allow us to do fast database-like operations
+#Dictionaries have different names in different languages
+    #Associative Arrays - Perl/php
+    #Properties or Map or HashMap -Java
+    #Property Bag-C#/.Net
+
+purse = dict()
+purse['money'] = 12
+purse['candy'] = 3
+purse['tissues'] = 75
+print(purse)
+print(purse['candy'])
+purse['candy'] = purse['candy'] + 2 
+print(purse)
+
