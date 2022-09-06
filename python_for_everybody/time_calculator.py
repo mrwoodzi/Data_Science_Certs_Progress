2f6278834a784f988f5d4558131eb3315446130e
mrwoodzi <mrwoodzi@protonmail.com>
2022-09-06 14:07:12 -0500

    The next one is starting

---
+++ python_for_everybody/time_calculator.py
@@ -0,0 +1,9 @@
+# need 2 required parameters and one optional
+# (1, 2, Day_of_Week=None)
+#What is the best way for program to know what day of the week it is?
+#If the result will be the next day, it should show (next day) after the time.
+#If the result will be more than one day later, it should show (n days later) after the time, where "n" is the number of days later.
+#All needs to be return in a string in " "
+
+
+
