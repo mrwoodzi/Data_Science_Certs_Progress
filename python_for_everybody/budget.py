3ee769b71a3c348ca1b57b21779ac9f2d79e0f2e
mrwoodzi <mrwoodzi@protonmail.com>
2022-09-13 15:46:08 -0500

    Yikes

--- python_for_everybody/budget.py
+++ python_for_everybody/budget.py
@@ -1,34 +1,78 @@
+# needs a ledger that is a list
+
+
 class Category:
-    pass 
-    def depost():
+    ledger = [] #class atribute
+    all = []
+    def __init__(self, food=None, clothing=None, auto=None, entertainment=None, business=None): # magic method
+        #Run Validations to the received arguments
 
-    def withdraw():
+        #Assign to self object
+        self.food = food
+        self.clothing = clothing
+        self.auto = auto
+        self.entertainment = entertainment
+        self.business = business
+
+        # Actions to execute
+        Category.all.append(self)
+
+
+    def deposit(self, deposit=float, sd=''):
+        self.deposit = self.deposit
+        pass
+        #self.ledger(deposit, s)
+        #return self.deposit
+
+    def withdraw(self, withdraw=float, sw=''):
+        self.withdraw = self.withdraw
+        print(sw, withdraw)
+        #withdraw = (withdraw/-1)
+        pass
+        #something possibly useful on oop py vid at 1hr 8min go double check
+        #balance += (money/-1)
+        #return self.withdraw
+
+    #def auto(self, auto=float, sa=''):
+        #print(sa)
+        #print(str(auto))
         pass
 
-    def get_balance():
+    def get_balance(self):
+        self.balance = self.deposit - self.withdraw
+        return self.balance 
+
+
+    def transfer(self, transfer, st=''):
         pass
 
-    def transfer():
+    def check_funds(self, check_funds):
+        pass
+    
+    def ledger(self): # instance variable, adds all transactions as a list
         pass
 
-    def check_funds():
-        pass
+    def __repr__(self):
+        return f"Category('{self.food}')"
 
-food = budget.Category("Food")
-food.deposit(1000, "initial deposit")
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
-print(food.get_balance())
-clothing = budget.Category("Clothing")
-food.transfer(50, clothing)
-clothing.withdraw(25.55)
-clothing.withdraw(100)
-auto = budget.Category("Auto")
-auto.deposit(1000, "initial deposit")
-auto.withdraw(15)
+#def create_spend_chart:
 
-print(food)
-print(clothing)
+food = Category("Food") #name
+food.deposit(1000, "initial deposit") #price
+food.withdraw(10.15, "groceries") #quantity
+food.withdraw(15.89, "restaurant and more food for dessert") #quantity
+#print(food.get_balance())
 
-print(create_spend_chart([food, clothing, auto]))
+clothing = Category("Clothing") #name
+food.transfer(50, "clothing") #quantity
+clothing.withdraw(25.55)#quantity
+clothing.withdraw(100)#quantity
+
+auto = Category("Auto") #name
+auto.deposit(1000, "initial deposit") #price
+auto.withdraw(15)#quantity
+print(Category.all)
+
+for instance in Category.all:
+    print(instance.all)
 
