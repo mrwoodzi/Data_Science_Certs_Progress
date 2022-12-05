2cfe9877083b2828fbe446ac4bb702e0fa516dd7
mrwoodzi <mrwoodzi@protonmail.com>
2022-12-05 16:25:13 -0600

    got it

--- data_analysis_with_Python/main.py
+++ data_analysis_with_Python/main.py
@@ -1,5 +1,14 @@
 import numpy as np
 
-new_matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+new_matrix = np.array([[1, 2, 3], 
+                       [4, 5, 6], 
+                       [7, 8, 9]
+                       ])
 
 print(new_matrix)
+nm_sum = new_matrix.sum(axis=0) # adds y axis for all
+print(nm_sum)
+
+a = np.arange(5)
+a + 20
+print(a)
