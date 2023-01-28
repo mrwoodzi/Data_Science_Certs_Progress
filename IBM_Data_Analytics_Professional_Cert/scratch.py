e1c98bde69d7cc8192b3453d2b4298858eaa9b39
mrwoodzi <mrwoodzi@protonmail.com>
2023-01-28 09:17:56 -0600

    Bash

---
+++ IBM_Data_Analytics_Professional_Cert/scratch.py
@@ -0,0 +1,14 @@
+a = int('E', 17)
+print(a)
+
+int function has two optional parameters, one the string representation of a integer value and 
+second a base. In this case E in base 17 its a number.
+the equivalence between base 10 and 17 is:
+10---17
+
+a = [1, 2, 3]
+y = a
+У[1]= 4
+print(a)
+this creates a SyntaxError maybe due to a shallow or deep copy issue 
+
