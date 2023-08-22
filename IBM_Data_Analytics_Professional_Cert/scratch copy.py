ad8731139e08fffc08cc48c86d8892aebc55f144
Michael Woods <mrwoodzi@protonmail.com>
2023-08-22 09:25:10 -0500

    Lots of linkedin this morning and starting a new chatgpt folder with questions to review

--- IBM_Data_Analytics_Professional_Cert/scratch copy.py
+++ IBM_Data_Analytics_Professional_Cert/scratch copy.py
@@ -1,29 +1,14 @@
-import pandas as pd
-import numpy as np
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
+    return wrapper
 
+def say_whee():
+    print("Whee!")
 
-def text_formatter(start, stop):
-    def delete_list_gen(start, stop):
-        l_array = np.linspace(start, stop)
-        float_list = l_array.astype(float)
-        round_list = float_list.round(1)
-        unique_list = np.unique(round_list)
-        py_list = str(unique_list.tolist())
-        quote_list = ["Save"]
-        for iter in unique_list:
-            quote_list.append("%s" % iter)
-        delete_list = quote_list
-        return delete_list
-    delete_list = delete_list_gen(start, stop)
-    #infile = "unformatted.txt"
-    #outfile = "cleaned_file.txt"
-    #with open(infile) as fin, open(outfile, "w+") as fout:
-        #for line in fin:
-            #for word in delete_list:
-                #line = line.replace(word, "")
-            #fout.write(line)
-    return delete_list
+say_whee = my_decorator(say_whee)
 
-
-print(text_formatter(1.5, 5.0))
+print(say_whee())
 
