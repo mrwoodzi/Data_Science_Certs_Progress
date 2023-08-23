9ab0078173831ae750daab3f1e55d909c054cc10
Michael Woods <mrwoodzi@protonmail.com>
2023-08-23 13:32:11 -0500

    I wrote a pdf to text to extract text script

--- Code_For_Campaigns/pdf_text_parser.py
+++ Code_For_Campaigns/pdf_text_parser.py
@@ -1,46 +1,61 @@
 import pypdf
 
-def extract_text_between_phrases(pdf_path, start_phrase, end_phrase, text_path):
-    # Open the PDF file
-    pdf_file = open(pdf_path, 'rb')
-    pdf_reader = pypdf.PdfFileReader(pdf_file)
+# Function to extract text from a PDF and save it to a text file
+def extract_text_to_txt(pdf_path, txt_path):
+    pdf_reader = pypdf.PdfReader(pdf_path)
     
-    # Initialize a flag to indicate if the start phrase is found
-    start_found = False
-    
-    # Initialize a string to store the extracted text
     extracted_text = ''
+    for page in pdf_reader.pages:
+        extracted_text += page.extract_text()
     
-    # Iterate through each page of the PDF
-    for page_num in range(pdf_reader.getNumPages()):
-        page = pdf_reader.getPage(page_num)
-        page_text = page.extractText()
-        
-        # Check if the start phrase is found in the current page
-        if start_phrase in page_text:
-            start_found = True
-        
-        # Extract text only if the start phrase is found
-        if start_found:
-            extracted_text += page_text
-            
-            # Check if the end phrase is found in the current page
-            if end_phrase in page_text:
-                break
-    
-    # Write the extracted text to a text file
-    with open(text_path, 'w', encoding='utf-8') as text_file:
-        text_file.write(extracted_text)
-    
-    # Close the PDF file
-    pdf_file.close()
+    with open(txt_path, 'w', encoding='utf-8') as txt_file:
+        txt_file.write(extracted_text)
 
 # Specify the paths for the PDF and text files
-pdf_path = 'input.pdf'
-text_path = 'output.txt'
+pdf_path = 'May_6_2023_PrecinctResults.pdf'
+txt_path = 'full_pdf_text.txt'
+
+# Extract text from the PDF and write to a text file
+extract_text_to_txt(pdf_path, txt_path)
+
+# Specify start and end phrases for extraction
 start_phrase = 'For Council, City of San Antonio, District 9'
 end_phrase = 'CITY OF SAN ANTONIO - PROPOSITION A'
 
-# Extract text between specified phrases
-# extract_text_between_phrases(pdf_path, start_phrase, end_phrase, text_path)
-extract_text_between_phrases(pdf_path, start_phrase, end_phrase, text_path)
+# Paths for input and output text files
+input_txt_path = 'full_pdf_text.txt'
+output_txt_path = 'extracted_text.txt'
+
+# Read the full text from the input file
+with open(input_txt_path, 'r', encoding='utf-8') as input_file:
+    full_text = input_file.read()
+
+# Initialize start index for searching
+start_index = 0
+
+# Loop to extract text between start and end phrases
+while start_index != -1:
+    # Find the start phrase from the current start index
+    start_index = full_text.find(start_phrase, start_index)
+    
+    if start_index == -1:
+        break
+    
+    # Find the end phrase from the current start index
+    end_index = full_text.find(end_phrase, start_index)
+    
+    if end_index == -1:
+        break
+    
+    # Extract the text between the phrases
+    extracted_text = full_text[start_index:end_index + len(end_phrase)]
+    
+    # Write the extracted text to the output file
+    with open(output_txt_path, 'a', encoding='utf-8') as output_file:
+        output_file.write(extracted_text + '\n')  # Adding a newline between extracted texts
+    
+    # Update the start index for the next iteration
+    start_index = end_index + len(end_phrase)
+
+# Print a message after extraction is complete
+print("Text extracted and saved to", output_txt_path)
