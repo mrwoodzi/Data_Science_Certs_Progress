4ae4d902c2dc32e5e36142ae7983a0ed4e5cddc8
Michael Woods <mrwoodzi@protonmail.com>
2023-08-30 16:13:55 -0500

    Starting a new folder where I am learning how to use python/pandas and display interactive data in powerbi

---
+++ PowerBi/powerbi_import.py
@@ -0,0 +1,27 @@
+# import powerbi
+
+# # Create a connection to Power BI
+# conn = powerbi.connect()
+
+# # Load the data from a CSV file
+# data = conn.load_data("data.csv")
+
+# # Create a report
+# report = conn.create_report(data)
+
+# # Run the report
+# report.run()
+
+# import powerbi
+
+# # Create a connection to Power BI Desktop
+# conn = powerbi.connect_desktop()
+
+# # Load the data from a CSV file
+# data = conn.load_data("data.csv")
+
+# # Create a report
+# report = conn.create_report(data)
+
+# # Run the report
+# report.run()
