c98be3c05b54f0242b19580c6dd5ec82b9360c95
Michael Woods <mrwoodzi@protonmail.com>
2023-11-08 10:13:12 -0600

    Just an update to robo club directory

--- robotics_club/notes_ideas.py
+++ robotics_club/notes_ideas.py
@@ -2,3 +2,5 @@
 # Ideas:
 # Learn Python 
 # https://www.toptal.com/robotics/programming-a-robot-an-introductory-tutorial 
+# github link for above tutorial https://github.com/nmccrea/sobot-rimulator
+
