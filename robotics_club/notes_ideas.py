5acc2cf6cd209b5a2ae2ea5f666b84f8c489b2f7
Michael Woods <77025481+mrwoodzi@users.noreply.github.com>
2023-11-09 17:13:54 -0600

    Update notes_ideas.py

--- robotics_club/notes_ideas.py
+++ robotics_club/notes_ideas.py
@@ -4,3 +4,32 @@
 # https://www.toptal.com/robotics/programming-a-robot-an-introductory-tutorial 
 # github link for above tutorial https://github.com/nmccrea/sobot-rimulator
 
+# Future: 
+
+#   Build Robots 
+#   -Learn SOLIDWORKS  
+#   -3d print 
+#   -learn the motors 
+#   -coding editor that works with the robotics languages 
+
+# Success: 
+#   Team Work 
+
+# Failures: 
+#     Construction Planning 
+#     -design errors 
+#     -stop and think what tools are available 
+#     -traction on wheels  
+#     -motors spinning at proper speed 
+#     Coding Editor Issues 
+#     -Learn One 
+#     -make sure it's compatible with robotics language 
+#     -have multiple computers on hand if one doesn't 
+
+# To Do List: 
+#     -Can we put a 3d printer in the meeting room 
+#     -Get guest internet pass 
+#     -Choose a New Competition to compete in for Spring 2024
+
+
+# Code: https://www.toptal.com/robotics/programming-a-robot-an-introductory-tutorial 
