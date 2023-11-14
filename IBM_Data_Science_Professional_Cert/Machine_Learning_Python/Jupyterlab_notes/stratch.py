71011f0d54d4d6804f137e098e5cabc20c099b85
Michael Woods <mrwoodzi@protonmail.com>
2023-11-14 12:00:31 -0600

    updates for pip and py libs

--- IBM_Data_Science_Professional_Cert/Machine_Learning_Python/Jupyterlab_notes/stratch.py
+++ IBM_Data_Science_Professional_Cert/Machine_Learning_Python/Jupyterlab_notes/stratch.py
@@ -1,2 +1,11 @@
-import numpy as np 
-np.polyval((x-5)(x-5))
+import numpy as np
+
+# Example coefficients of a quadratic polynomial: x^2 - 10x + 25
+coefficients = [1, -10, 25]
+
+# Evaluate the polynomial at the point (x-5)(x-5)
+point = np.array([5])  # The point (x-5)(x-5) for x = 5
+result = np.polyval(coefficients, point)
+
+print(result)
+
