38c7021d150b5b907780c062dbb045824eca5165
Michael Woods <mrwoodzi@protonmail.com>
2023-12-14 11:12:43 -0600

    School Semester almost done so I can get back to my own studies

---
+++ scratch.py
@@ -0,0 +1,26 @@
+# name = input("Who are you? ")
+# print("Hello", name)
+
+# x = 15
+# y = 5
+
+# success = False
+# if x < 10 and y > 10:
+#     print("success!")
+#     success = True
+# else:
+#     x = int(input("Enter a number: "))
+#     y = int(input("Enter another number: "))
+
+#     if type(x) != int or type(y) != int:
+#         print("Please enter an integer for both numbers.")
+#     else:
+#         print("Numbers entered successfully.")
+
+
+x = 3
+print(x)
+
+# I want to add 2 to x
+x = x + 2
+print(x)
