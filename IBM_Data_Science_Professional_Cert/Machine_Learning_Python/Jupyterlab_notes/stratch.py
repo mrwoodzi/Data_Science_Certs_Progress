a77d67976caf71c3cb533b25a33086133c44bf4e
Michael Woods <mrwoodzi@protonmail.com>
2024-01-15 13:40:37 -0600

    Just doing a little Physics with Python

--- IBM_Data_Science_Professional_Cert/Machine_Learning_Python/Jupyterlab_notes/stratch.py
+++ IBM_Data_Science_Professional_Cert/Machine_Learning_Python/Jupyterlab_notes/stratch.py
@@ -1,11 +1,6 @@
-import numpy as np
 
-# Example coefficients of a quadratic polynomial: x^2 - 10x + 25
-coefficients = [1, -10, 25]
-
-# Evaluate the polynomial at the point (x-5)(x-5)
-point = np.array([5])  # The point (x-5)(x-5) for x = 5
-result = np.polyval(coefficients, point)
-
-print(result)
+a = 9
+b = 5
+c = (a**2 + b**2)**0.5
+print(c)
 
