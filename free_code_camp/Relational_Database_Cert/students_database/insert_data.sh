b702efeabdadbf910c7bed70e2860095270d825d
Michael Woods <mrwoodzi@protonmail.com>
2024-02-27 12:36:53 -0600

    I figured out how to run a postgreSQL database locally to copy what I am doing in my freecodecamp.org relational database certification online

---
+++ free_code_camp/Relational_Database_Cert/students_database/insert_data.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Script to insert data from courses.csv and students.csv into students database
+# majors table
+PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+
+cat courses_test.csv | while IFS="," read MAJOR COURSE
+do
+  if [[ $MAJOR != major ]]
+  then
+  # get major_id
+  MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+
+  # if not found
+  if [[ -z $MAJOR_ID ]]
+  then
+    # insert major
+    INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
+    if [[ $INSERT_MAJOR_RESULT == "INSERT 0 1" ]]
+    then
+      echo "Inserted into majors, $MAJOR"
+    fi
+  # get new major_id
+  MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+
+  fi
+  # get course_id
+  COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
+
+  # if not found
+  if [[ -z $COURSE_ID ]]
+  then
+    
+
+
+  fi
+
+  # insert course
+
+  # get new course_id
+
+  # insert into majors_courses
+
+  fi
+done
