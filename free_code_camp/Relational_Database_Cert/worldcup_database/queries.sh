ec91f0c7d5420bea5038da7b1686a13abb446e03
Michael Woods <mrwoodzi@protonmail.com>
2024-04-29 08:58:26 -0500

    things from last week

---
+++ free_code_camp/Relational_Database_Cert/worldcup_database/queries.sh
@@ -0,0 +1,40 @@
+#teams table has columns team_id, name
+#games table has columns year, round, winner_id, loser_id, winner_goals, loser_goals
+# Do not change code above this line. Use the PSQL variable above to query your database.
+# This is for  POSTGRESQL
+echo -e "\nTotal number of goals in all games from winning teams:"
+echo "$($PSQL "SELECT SUM(winner_goals) FROM games")"
+
+echo -e "\nTotal number of goals in all games from both teams combined:"
+echo "$($PSQL "SELECT SUM(winner_goals + loser_goals) FROM games")"
+
+echo -e "\nAverage number of goals in all games from the winning teams:"
+echo "$($PSQL "SELECT AVG(winner_goals) FROM games")"
+
+echo -e "\nAverage number of goals in all games from the winning teams rounded to two decimal places:"
+echo "$($PSQL "SELECT ROUND(AVG(winner_goals), 2) FROM games")"
+
+echo -e "\nAverage number of goals in all games from both teams:"
+echo "$($PSQL "SELECT AVG(winner_goals + loser_goals) FROM games")"
+
+echo -e "\nMost goals scored in a single game by one team:"
+echo "$($PSQL "SELECT MAX(winner_goals) FROM games")"
+
+echo -e "\nNumber of games where the winning team scored more than two goals:"
+echo "$($PSQL "SELECT COUNT(winner_goals) FROM games WHERE winner_goals > 2")"
+
+echo -e "\nWinner of the 2018 tournament team name:"
+#echo "$($PSQL "SELECT name FROM teams WHERE team_id = (SELECT winner_id FROM games WHERE year = 2018)")"
+#echo "$($PSQL "SELECT name FROM teams JOIN games ON teams.team_id = games.winner_id WHERE year = 2018")"
+
+echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
+echo
+
+echo -e "\nList of unique winning team names in the whole data set:"
+echo
+
+echo -e "\nYear and team name of all the champions:"
+echo
+
+echo -e "\nList of teams that start with 'Co':"
+echo
