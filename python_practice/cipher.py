d55d43898bbb4352777f7e6e846229e2ebf6c17b
mrwoodzi <mrwoodzi1ba@gmail.com>
2024-06-18 10:15:59 -0500

    Saving work from yesterday

--- python_practice/cipher.py
+++ python_practice/cipher.py
@@ -1,13 +1,19 @@
-text = 'Hello World'
-shift = 3
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-encrypted_text = ''
+text = 'Hello Zaira'
+custom_key = 'python'
 
-for char in text.lower():
-    if char == ' ':
-        encrypted_text += char
-    else
-        index = alphabet.find(char)
-        new_index = index + shift
-        encrypted_text += alphabet[new_index]
-    print('char:', char, 'encrypted text:', encrypted_text)
+def vigenere(message, key):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+
+    for char in message.lower():
+        if char == ' ':
+            encrypted_text += char
+        else:
+            index = alphabet.find(char)
+            new_index = (index + key) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    print('plain text:', message)
+    print('encrypted text:', encrypted_text)
+
+# Vigenere cipher 
+    # where the key for each letter is determined by another text, called the key 
