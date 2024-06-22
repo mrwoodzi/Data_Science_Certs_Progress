ebbaba7a0b9b2c13e09c25eaef55578f3cab7435
mrwoodzi <mrwoodzi1ba@gmail.com>
2024-06-22 08:37:02 -0500

    Little bit of RDBMS

--- free_code_camp/Relational_Database_Cert/bike_rental_shop/bike-shop.sh
+++ free_code_camp/Relational_Database_Cert/bike_rental_shop/bike-shop.sh
@@ -76,22 +76,34 @@ RENT_MENU() {
         fi
 
         # get customer_id
-        CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$PHONE_NUMBER';")
-        # insert bike rental 
-        INSERT_RENTAL_RESULT=$($PSQL "INSERT INTO rentals(bike_id, customer_id) VALUES('$BIKE_ID_TO_RENT', '$CUSTOMER_ID');")
+        CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$PHONE_NUMBER'")
+
+        # insert bike rental
+        INSERT_RENTAL_RESULT=$($PSQL "INSERT INTO rentals(customer_id, bike_id) VALUES($CUSTOMER_ID, $BIKE_ID_TO_RENT)")
+
         # set bike availability to false
+        SET_TO_FALSE_RESULT=$($PSQL "UPDATE bikes SET available = false WHERE bike_id = $BIKE_ID_TO_RENT")
 
-        # get bike info 
-
+        # get bike info
+        BIKE_INFO=$($PSQL "SELECT size, type FROM bikes WHERE bike_id = $BIKE_ID_TO_RENT")
+        BIKE_INFO_FORMATTED=$(echo $BIKE_INFO | sed 's/ |/"/')
+        
         # send to main menu
-
+        MAIN_MENU "I have put you down for the $BIKE_INFO_FORMATTED Bike, $(echo $CUSTOMER_NAME | sed -E 's/^ *| *$//g')."
       fi
     fi
   fi
 }
 
 RETURN_MENU() {
-  echo "Return Menu"
+            # get customer info 
+            echo -e "\nWhat's your phone number?"
+            read PHONE_NUMBER 
+            CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers where phone = '$PHONE_NUMBER'")
+            # if not found 
+
+
+            # send to main menu 
 }
 
 EXIT() {
